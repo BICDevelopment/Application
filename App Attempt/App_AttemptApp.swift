11e8c504e12da1644926886b97b350076444afaa
Devin Bickford <ddbickford05@gmail.com>
2022-07-16 18:10:37 -0400

    Initial Commit

---
+++ App Attempt/App_AttemptApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_AttemptApp.swift
+//  App Attempt
+//
+//  Created by Devin Bickford on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct App_AttemptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
